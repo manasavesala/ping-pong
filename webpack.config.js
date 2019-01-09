97b65957bc6a7b998e47c8f0b1dbcd5f309d1183
Manasa Vesala <vesalamanasa@gmail.com>
2019-01-09 07:50:36 -0800

    Added .eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -9,7 +9,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval-source-map',
+  devServer: {
+   contentBase: './dist'
+  },
   plugins: [
+
+    new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Ping Pong',
@@ -17,6 +23,7 @@ module.exports = {
       inject: 'body'
     })
   ],
+
   module: {
     rules: [
       {
@@ -25,6 +32,11 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
       }
     ]
   }
